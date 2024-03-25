27eda23bd4a3e61291587bc8d5e3bdaa29dfb9d8
Swan Yee Tun <swanyeetun.2000@gmail.com>
2024-03-25 08:18:48 +0630

    task done status add

--- commands/list.js
+++ commands/list.js
@@ -2,118 +2,131 @@ import todo from "../models/todo.js";
 import Table from "cli-table";
 import chalk from "chalk";
 import inquirer from "inquirer";
-import db from "../database/db.js";
 
-const todoLists = async function (pageNumber = 1, pageSize = 5, is_done = null, search = null) {
-    try {
-        const tasks = await todo.getAll(pageNumber, pageSize, is_done, search);
+const todoLists = async function (
+  pageNumber = 1,
+  pageSize = 5,
+  is_done = null,
+  search = null
+) {
+  try {
+    const tasks = await todo.getAll(pageNumber, pageSize, is_done, search);
 
-        if (tasks.length === 0) {
-            console.log(chalk.red("No tasks found."));
-            return null;
-        }
-
-        const totalCount = await todo.countTasks(is_done, search);
-        console.log(chalk.blue(`Total Count: ${totalCount} | Current Page: ${pageNumber}`));
-
-        displayTasks(tasks);
-
-        await viewDetailTask(tasks);
-    } catch (error) {
-        console.error('Error fetching todo lists:', error);
-        return null;
+    if (tasks.length === 0) {
+      console.log(chalk.red("No tasks found."));
+      return null;
     }
-}
+
+    const totalCount = await todo.countTasks(is_done, search);
+    console.log(
+      chalk.blue(`Total Count: ${totalCount} | Current Page: ${pageNumber}`)
+    );
+
+    displayTasks(tasks);
+
+    await viewDetailTask(tasks);
+  } catch (error) {
+    console.error("Error fetching todo lists:", error);
+    return null;
+  }
+};
 
 function displayTasks(tasks) {
-    const table = generateTable(tasks);
-    console.log(table.toString());
+  const table = generateTable(tasks);
+  console.log(table.toString());
 }
 
 function generateTable(tasks) {
-    try {
-        const table = new Table({
-            head: ['Task Name', 'Priority','is Done','Finished Date Time'],
-            colWidths: [20, 20, 20, 20],
-            wordWrap: true,
-            wrapOnWordBoundary: false,
-        })
+  try {
+    const table = new Table({
+      head: ["Task Name", "Priority", "is Done", "Finished Date Time"],
+      colWidths: [20, 20, 20, 20],
+      wordWrap: true,
+      wrapOnWordBoundary: false,
+    });
 
-        tasks.forEach(task => {
-            const priorityColor = getPriorityColor(task.priority_level);
-            const statusIcon = task.is_doned ? "✅" : "🛑";
-            const finishedDateTime = task.done_at ? new Date(task.done_at).toDateString() : "N/A";
-            
-            table.push([
-                task.name,
-                priorityColor,
-                statusIcon,
-                finishedDateTime,
-            ])
-        });
-        
-        return table;
-    } catch (error) {
-        console.error('Error generating table:', error);
-        return null;
-    }
+    tasks.forEach((task) => {
+      const priorityColor = getPriorityColor(task.priority_level);
+      const statusIcon = task.is_doned ? "✅" : "🛑";
+      const finishedDateTime = task.done_at
+        ? new Date(task.done_at).toDateString()
+        : "N/A";
+
+      table.push([task.name, priorityColor, statusIcon, finishedDateTime]);
+    });
+
+    return table;
+  } catch (error) {
+    console.error("Error generating table:", error);
+    return null;
+  }
 }
 
 function getPriorityColor(priorityLevel) {
-    switch (priorityLevel) {
-        case 'Low':
-            return chalk.white(priorityLevel);
-        case 'Medium':
-            return chalk.yellow(priorityLevel);
-        case 'High':
-            return chalk.red(priorityLevel);
-        default:
-            return chalk.white(priorityLevel);
-    }
+  switch (priorityLevel) {
+    case "Low":
+      return chalk.white(priorityLevel);
+    case "Medium":
+      return chalk.yellow(priorityLevel);
+    case "High":
+      return chalk.red(priorityLevel);
+    default:
+      return chalk.white(priorityLevel);
+  }
 }
 
 async function viewDetailTask(tasks) {
-    const choices = tasks.map(task => ({
-        name: `${chalk.red(task.id)} ${chalk.blue(task.name)}`,
-        value: task
-    }));
+  const choices = tasks.map((task) => ({
+    name: `${chalk.red(task.id)} ${chalk.blue(task.name)}`,
+    value: task,
+  }));
 
-    const { selectedTask } = await inquirer.prompt({
-        type: 'list',
-        name: 'selectedTask',
-        message: 'Select a task to view details:',
-        choices: choices
-    });
+  const { selectedTask } = await inquirer.prompt({
+    type: "list",
+    name: "selectedTask",
+    message: "Select a task to view details:",
+    choices: choices,
+  });
 
-    displayTaskDetail(selectedTask);
+  displayTaskDetail(selectedTask);
 }
 
 async function displayTaskDetail(task) {
-    console.log(chalk.green("Task Details:"));
-    console.log(chalk.blue("ID: "), task.id);
-    console.log(chalk.blue("Name: "), task.name);
-    console.log(chalk.blue("Description: "), task.description || "N/A");
-    console.log(chalk.blue("Status: "), task.is_doned ? "Completed" : "Incomplete");
-    console.log(chalk.blue("Priority Level: "), getPriorityColor(task.priority_level));
-    console.log(chalk.blue("Finished Date Time: "), task.done_at ?  new Date(task.done_at) : "N/A");
+  console.log(chalk.green("Task Details:"));
+  console.log(chalk.blue("ID: "), task.id);
+  console.log(chalk.blue("Name: "), task.name);
+  console.log(chalk.blue("Description: "), task.description || "N/A");
+  console.log(
+    chalk.blue("Status: "),
+    task.is_doned ? "Completed" : "Incomplete"
+  );
+  console.log(
+    chalk.blue("Priority Level: "),
+    getPriorityColor(task.priority_level)
+  );
+  console.log(
+    chalk.blue("Finished Date Time: "),
+    task.done_at ? new Date(task.done_at) : "N/A"
+  );
 
-    const message = task.is_doned ? "Wanna undone?" : "Are you done?";
+  const message = task.is_doned ? "Wanna undone?" : "Are you done?";
 
-   await inquirer.prompt([
-    {
-      type: 'confirm',
-      name: 'confirmed',
-      message: message
-    }
-  ])
-  .then(async (answers) => {
+  await inquirer
+    .prompt([
+      {
+        type: "confirm",
+        name: "confirmed",
+        message: message,
+      },
+    ])
+    .then(async (answers) => {
       if (answers.confirmed) {
         await todo.doneTask(task.id);
-        console.log('doned...');
+        console.log("done...");
       } else {
-            console.log('cancled...');
-        }
-  });
+        console.log("canceled...");
+      }
+    });
 }
 
 export default todoLists;
--- models/todo.js
+++ models/todo.js
@@ -1,60 +1,60 @@
 import db from "../database/db.js";
 
 export default class Todo {
-    static async getAll(pageNumber, pageSize, isDone, search) {
-        try {
-            const offset = (pageNumber - 1) * pageSize;
+  static async getAll(pageNumber, pageSize, isDone, search) {
+    try {
+      const offset = (pageNumber - 1) * pageSize;
 
-            return await db('tasks')
-                .select('*')
-                .where((queryBuilder) => {
-                    this.applyFilters(queryBuilder, isDone, search);
-                })
-                .orderBy('created_at', 'desc')
-                .limit(pageSize)
-                .offset(offset);
-        } catch (error) {
-            console.error('Error fetching tasks:', error);
-            throw error;
-        }
+      return await db("tasks")
+        .select("*")
+        .where((queryBuilder) => {
+          this.applyFilters(queryBuilder, isDone, search);
+        })
+        .orderBy("created_at", "desc")
+        .limit(pageSize)
+        .offset(offset);
+    } catch (error) {
+      console.error("Error fetching tasks:", error);
+      throw error;
+    }
+  }
+
+  static async countTasks(isDone = null, search = null) {
+    try {
+      return db("tasks")
+        .where((queryBuilder) => {
+          this.applyFilters(queryBuilder, isDone, search);
+        })
+        .count()
+        .then((result) => {
+          return result[0]["count(*)"];
+        });
+    } catch (error) {
+      console.error("Error fetching tasks:", error);
+      throw error;
+    }
+  }
+
+  static async setTask(tasks) {
+    return await db("tasks").insert(tasks);
+  }
+
+  static applyFilters(queryBuilder, isDone, search) {
+    if (isDone != null) {
+      queryBuilder.where("is_doned", isDone == "true" ? true : false);
     }
 
-    static async countTasks(isDone = null, search = null) {
-        try {
-            return db('tasks')
-                    .where((queryBuilder) => {
-                        this.applyFilters(queryBuilder, isDone, search);
-                    })
-                    .count()
-                    .then((result) => {
-                        return result[0]['count(*)'];
-                    });
-        } catch (error) {
-            console.error('Error fetching tasks:', error);
-            throw error;
-        }
+    if (search) {
+      queryBuilder.where("name", "like", `%${search}%`);
     }
+  }
 
-    static async setTask(tasks) {
-        return await db('tasks').insert(tasks);
-    }
-
-    static applyFilters(queryBuilder, isDone, search) {
-        if (isDone != null) {
-            queryBuilder.where('is_doned', (isDone == 'true') ? true : false);
-        }
-
-        if (search) {
-            queryBuilder.where('name', 'like', `%${search}%`);
-        }
-    }
-
-    static async doneTask(taskId) {
-        return (await db('tasks')
-                    .where('id',taskId)
-                    .update({
-                        is_doned: db.raw('NOT ??', ['is_doned']),
-                        done_at:  db.raw('CASE WHEN is_doned THEN NULL ELSE datetime("now") END')
-                    }));
-    }
+  static async doneTask(taskId) {
+    return await db("tasks")
+      .where("id", taskId)
+      .update({
+        is_doned: db.raw("NOT ??", ["is_doned"]),
+        done_at: new Date().toISOString(),
+      });
+  }
 }
